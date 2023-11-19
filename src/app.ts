b6e533a2a28e1047ff1e064aacdeb295e2f774df
Mohammad Rafe <mrrafi5869@gmail.com>
2023-11-19 09:58:31 +0600

    project-setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+console.log(process.cwd());
+export default app;
